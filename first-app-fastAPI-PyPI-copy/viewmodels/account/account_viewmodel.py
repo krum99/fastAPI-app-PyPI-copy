adbbdef20f912cc15b830a7a13fa7060adb76867
krum99 <79230210+krum99@users.noreply.github.com>
2025-08-02 00:54:34 +0300

    chpater 7-11 : (refactor) get the user from the service instead of hardcoded value in AccountViewModel

--- first-app-fastAPI-PyPI-copy/viewmodels/account/account_viewmodel.py
+++ first-app-fastAPI-PyPI-copy/viewmodels/account/account_viewmodel.py
@@ -1,4 +1,6 @@
 from starlette.requests import Request
+
+from services import user_service
 from viewmodels.shared.viewmodel import ViewModelBase
 from data.user import User
 
@@ -6,4 +8,5 @@ from data.user import User
 class AccountViewModel(ViewModelBase):
     def __init__(self, request: Request):
         super().__init__(request)
-        self.user = User('Krum', 'krum@myemail.com', 's65dvsdbds91v')
+
+        self.user = user_service.get_user_by_id(self.user_id)
